8213169a5bcd2b9eb5ef86281feb0d9ee6ba1972
Ryosuke <ryosuke.san.hana@gmail.com>
2019-04-13 11:44:25 -0700

    :tada: sketchapp setup. exports JSON, no import

---
+++ src/pages/components.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Base } from 'pure-styled-components'
+import Button from 'react-bootstrap/Button';
+
+const IndexPage = () => (
+  <div>
+      <link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>
+      <div data-sketch-symbol="Button/Primary">
+        <Base>
+          <Button bg="red">Test</Button>
+        </Base>
+      </div>
+      </div>
+)
+
+export default IndexPage
